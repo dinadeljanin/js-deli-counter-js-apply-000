e6398cb5f65a5dddadc23e482443b9dfd9b101f1
D D <ddeljanin5@icloud.com>
2020-03-04 00:47:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
+// issue ticket nums
+// add cust num
+// say "welcome, you're ticket num"
 
-function takeANumber(katzDeliLine, customer) {
-  katzDeliLine.push(customer);
-  return `Welcome, ${customer}. You are number ${katzDeliLine.length} in line.`; 
+function takeANumber(line) { // line is an array
+  let ticketNums = 1;
+  line.push(ticketNums);
+  return `Welcome, you are ticket number ${(line.length)+1} in line`;
 }
+
+// function takeANumber(katzDeliLine, customer) {
+  
+//   katzDeliLine.push(customer);
+//   return `Welcome, ${customer}. You are number ${katzDeliLine.length} in line.`; 
+// }
 
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length === 0) {
@@ -19,6 +29,7 @@ function currentLine(line) {
   } else {
     for (let i = 0; i < line.length; i++) {
       lunchRush.push(`${(i + 1)}. ${line[i]}`);
-    } 
-  } return `The line is currently: ${lunchRush.join(', ')}`;  
+    }  
+  } 
+ return `The line is currently: ${lunchRush.join(', ')}`;
 }
